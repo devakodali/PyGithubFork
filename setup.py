6f59d9f744c0cbe0fe15102498cf2b5703058e95
Deva-Kodali <Deva.Kodali@ibm.com>
2020-11-13 11:44:46 -0600

    update token and name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='githubfork',
+    name='devatestPyGithubFork',
     version='default',
     description='Code wraps PyGithub to simplify workflows dealing with forks',
     author='hassenius',
-    url='https://github.com/multicloud-ops/PyGithubFork',
+    url='https://github.com/devakodali/PyGithubFork',
     packages=['PyGithubFork'],
     # install_requires=[
     #     'pyyaml',
