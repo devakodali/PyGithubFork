2aea7d285f9f8d361f455521305a2d2493e31b21
Deva-Kodali <Deva.Kodali@ibm.com>
2020-11-13 11:19:06 -0600

    add files to help publish to pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='githubfork',
+    version='default',
+    description='Code wraps PyGithub to simplify workflows dealing with forks',
+    author='hassenius',
+    url='https://github.com/multicloud-ops/PyGithubFork',
+    packages=['PyGithubFork'],
+    # install_requires=[
+    #     'pyyaml',
+    #     'pygithub'
+    # ]
+)
