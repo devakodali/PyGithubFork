7207eaf520817f74e77ef42bcb21307a9d519426
Deva-Kodali <Deva.Kodali@ibm.com>
2020-11-16 10:18:54 -0600

    change author to multicloud-ops

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='devatestPyGithubFork',
     version='default',
     description='Code wraps PyGithub to simplify workflows dealing with forks',
-    author='hassenius',
+    author='multicloud-ops',
     url='https://github.com/devakodali/PyGithubFork',
     packages=['githubfork']
     # install_requires=[
